8e8c87429ae1ba7850bad9f2206c52c022516b5a
Perry Neal <perryneal11@gmail.com>
2021-01-20 18:27:18 -0500

    inital commit
    general ui layout

---
+++ Clean Cars/Clean Cars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clean Cars
+//
+//  Created by Neal, Perry (P.) on 1/20/21.
+//  Copyright © 2021 Neal, Perry (P.). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
