abe3d557225773b93e9ed71c1a87775f04edfab1
Perry Neal <perryneal11@gmail.com>
2021-01-22 10:31:28 -0500

    terms and privacy policy
    user has to agree, and can view privacy policy and agreement

--- Clean Cars/Clean Cars/SignUpViewController.swift
+++ Clean Cars/Clean Cars/SignUpViewController.swift
@@ -14,40 +14,42 @@ class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
-        
-        
-        
     }
 
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var passwordConfirm: UITextField!
+    @IBOutlet weak var agreement: UISwitch!
     
     @IBAction func aignUpAction(_ sender: Any) {
-        if password.text != passwordConfirm.text {let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
+        
+        if agreement.isOn == false {
+            let alertController = UIAlertController(title: "Service Agreement", message: "Please agree to terms and service and privacy policy", preferredStyle: .alert)
             let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                    
-        alertController.addAction(defaultAction)
-        self.present(alertController, animated: true, completion: nil)
-                }else{
-        Auth.auth().createUser(withEmail: email.text!, password: password.text!){ (user, error) in if error == nil {
-           self.performSegue(withIdentifier: "signupToHome", sender: self)
-                        }
-         else{
-           let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
-           let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                            
             alertController.addAction(defaultAction)
             self.present(alertController, animated: true, completion: nil)
-               }
+        }
+        else{
+            if password.text != passwordConfirm.text {
+                let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                self.present(alertController, animated: true, completion: nil)
+                }
+            else {
+                Auth.auth().createUser(withEmail: email.text!, password: password.text!){
+                    (user, error) in if error == nil {
+                        self.performSegue(withIdentifier: "signupToHome", sender: self)
                     }
-              }
-        
-        
+                    else{
+                        let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                        alertController.addAction(defaultAction)
+                        self.present(alertController, animated: true, completion: nil)
+                    }
+                }
+            }
+        }
     }
-    
-    
 }
 
