c03ec402fb5018a744330efbf925282d9beecd2d
Perry Neal <perryneal11@gmail.com>
2021-01-27 16:46:16 -0500

    Cleanings in table WIP
    Have lceanings attempting to load in a table, work in progress.

--- Clean Cars/Clean Cars/HomeViewController.swift
+++ Clean Cars/Clean Cars/HomeViewController.swift
@@ -10,34 +10,59 @@ import UIKit
 import FirebaseDatabase
 import FirebaseAuth
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource {
+    let ref: DatabaseReference! = Database.database().reference()
+    let userID = Auth.auth().currentUser?.uid
+    var cleaningDictionary: [String : String] = [:]
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        var ref: DatabaseReference!
-        ref = Database.database().reference()
-        let userID = Auth.auth().currentUser?.uid
-        var cleanings = [String]()
-        var message = String()
-        
-        
-        
-        ref.child("users").child(userID!).child("cleanings").child("1").observeSingleEvent(of: .value) { (DataSnapshot) in
-            print(DataSnapshot)
-            let cleaning = DataSnapshot.value as? String
-            if let actualCleaning = cleaning{
-                cleanings.append(actualCleaning)
-                print(cleanings)
-                
+
+        ref.child("users").child(userID!).child("cleanings").observeSingleEvent(of: .value) { (DataSnapshot) in
+                print(DataSnapshot)
+
+                let enumerator = DataSnapshot.children
+                while let rest = enumerator.nextObject() as? DataSnapshot {
+                    let cleaningDictionary = rest.value as? [String : AnyObject] ?? [:]
+                }
+                DispatchQueue.main.async {
+                    self.tableView.reloadData();
+                }
             }
-            
         }
+    
+        func tableView(_ tableView:UITableView!, numberOfRowsInSection section:Int) -> Int
+         {
+            return cleaningDictionary.count + 1
+         }
         
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           // Reuse or create a cell.
+           let cell = tableView.dequeueReusableCell(withIdentifier: "cleaningCell", for: indexPath)
+
+            if cleaningDictionary.isEmpty{
+                cell.textLabel!.text = "You have no cleanings scheduled"
+            }
+            else if !cleaningDictionary.isEmpty{
+                var cleaningString: String = "Hello"
+                cell.textLabel!.text = cleaningString
+
+
+            }
+
+
+           return cell
+        }
+    
+
+  
+
+
         
         
     }
 
-}
 
